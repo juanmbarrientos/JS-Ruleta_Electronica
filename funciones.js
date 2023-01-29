b38ba33f9d0510e874c47739b9e3fda83e8e51f4
Juan Barrientos <109054384+juanmbarrientos@users.noreply.github.com>
2023-01-29 11:06:26 -0300

    Actualización lógica js

--- funciones.js
+++ funciones.js
@@ -1,16 +1,28 @@
-        
-        
+ 
+        // Crea el numero ramdon
         let numRandom2 = (Math.round(Math.random()*(36-0)+0));
+        
+        // llama al galery = tablero
         const gallery = document.getElementById('gallery')
+        
+        // donde se pushean los numeros que se guardan del tablero
         const arraySelecionado = [];
+        
+        //  donde se aloja el que sale
         const nResultados = [];
-        const contar = nResultados.push(numRandom2);
 
-       
+        // REVISAR
+        const contar = nResultados.push(numRandom2);
+    
+        // Transfiere los numero elegidos al arraySeleccionado.
+        let numSelecionado = "";
+
+
 
         // RECARGA LA PÁGINA Y EJECUTA EL RANDOM
         // setInterval("location.reload()",8000);
                
+        // FUNCION QUE DA EL RESULTADO DEL NUMERO SALIENTE
         function categorizar(){
             let colorear = document.getElementById("resultado");
             if (numRandom2 >= 1 && numRandom2 <= 36){
@@ -27,73 +39,70 @@
                 colorear.className = "verde";
             };
 
+            // coloca el resultado en pantalla
             document.getElementById("resultado").innerHTML=numRandom2;
+
+            // guarda el resultado en el localStorage, usa nResultados para no sobreescribir
             localStorage.setItem(nResultados, JSON.stringify(numRandom2));
         }      
         
+        
+        // para seleccionar en el tablero
         gallery.addEventListener('click', (e) => {
             console.log(e.target.textContent);
             e.target.classList.add("selec");
-            const numSelecionado = parseInt(e.target.textContent);
+            numSelecionado = parseInt(e.target.textContent);
             arraySelecionado.push(numSelecionado);
         })
         
 
-        let pesos = 0;
+        let pesos = 100;
         let nuevoSaldo = pesos;
-        let ganador = 35;
+        let ganador = 36;
         let saldo = document.getElementById("saldo").textContent = nuevoSaldo;
 
         function validar_jugada(){
+            let consumoFichas = nuevoSaldo - arraySelecionado.length;
             if (nuevoSaldo <= 0){
-                console.log("No tienes saldo para jugar")
+                alert("No tienes saldo para jugar")
             }else{
                 if ((arraySelecionado.includes(numRandom2) === true)){
                     alert("Eres un Ganador");
-                    console.log(nuevoSaldo - arraySelecionado.length)
-                    console.log(nuevoSaldo = ganador + pesos);
+                    console.log(nuevoSaldo = consumoFichas + ganador);
                 }else{
-                    console.log(saldo - arraySelecionado.length); 
                     alert("Seguir participando");
+
+                    console.log(consumoFichas); 
                 }
             }
         }
 
+// CONTADOR
+let tiempo = document.getElementById("tiempo_restante");
+let mensaje = document.getElementById("mensaje_tiempo");
+let contenedor_resultado = document.getElementById("contenedor_resultado")
+
+let cont = 30;
+
+setInterval(() =>{
+    tiempo.innerHTML = cont--;
+
+    if(cont == 29){
+        mensaje.innerHTML = "Hacé tus apuestas";
+        mensaje.classList.add("hace_apuestas")
+    }else if(cont == 10){
+        mensaje.innerHTML = "Te queda poco tiempo para apostar ¡Buena Suerte!";
+        mensaje.classList.add("poco_tiempo");
+    }else if (cont == 3){
+        mensaje.innerHTML = "¡No va más!";
+        mensaje.classList.add("no_va_mas");
+    }else if (cont == 0){
+        location.reload(); 
+    }
+},1000)
 
 
 
-        // let pesos = 100;
-        // let nuevoSaldo = pesos;
-        // let ganador = 35;
-        // let saldo = document.getElementById("saldo").textContent = nuevoSaldo;
 
-        // function validar_jugada(){
-        //     if ((arraySelecionado.includes(numRandom2) === true)){
-        //         alert("Eres un Ganador");
-        //         console.log(nuevoSaldo = ganador + pesos);
-        //     }else{
-        //         console.log(saldo - arraySelecionado.length); 
-        //         alert("Seguir participando");
-        //     }
-        // }
-    
-
-
-
-
-        // let pesos = 20;
-        // let saldo = document.getElementById("saldo").textContent = pesos;
-        // let ganador = 35;
-
-        // function validar_jugada(){
-        //     if ((arraySelecionado.includes(numRandom2) === true)){
-        //         alert("Eres un Ganador");
-        //         saldo = pesos + ganador;
-        //     }else{
-        //         alert("Seguir participando")
-        //     }
-        // }
-    
-        
 
 
