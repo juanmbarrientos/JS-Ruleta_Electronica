9250a0253d090ff2422e33d919bd494707627d8f
Juan Barrientos <109054384+juanmbarrientos@users.noreply.github.com>
2023-01-30 13:17:25 -0300

    Actualización de resultados + lógica

--- js/funciones.js
+++ js/funciones.js
@@ -1,91 +1,30 @@
- 
-        
-        // llama al galery = tablero
-        const gallery = document.getElementById('gallery')
-        
-        // Crea el numero ramdon
-        let numRandom2 = (Math.round(Math.random()*(36-0)+0));
-        
-        // donde se pushean los numeros que se guardan del tablero
-        const arraySelecionado = [];
-        
-        //  donde se aloja el que sale
-        const nResultados = [];
+// REVISAR
+// const contar = nResultados.push(numRandom2);
 
-        // REVISAR
-        const contar = nResultados.push(numRandom2);
-    
-        // Transfiere los numero elegidos al arraySeleccionado.
-        let numSelecionado = "";
+// Donde se pushean los numeros que se guardan del tablero.
+const arraySelecionado = [];
 
-        // RECARGA LA PÁGINA Y EJECUTA EL RANDOM
-        // setInterval("location.reload()",8000);
-               
-        // FUNCION QUE DA EL RESULTADO DEL NUMERO SALIENTE
-        function categorizar(){
-            let colorear = document.getElementById("resultado");
-            if (numRandom2 >= 1 && numRandom2 <= 36){
-                if (numRandom2%2 == 0){
-                    document.getElementById("categorizado").innerHTML=`El numero es ${numRandom2} ROJO - PAR`;
-                    colorear.className = "rojo";
-                }else{
-                    document.getElementById("categorizado").innerHTML=`El numero es ${numRandom2} NEGRO - IMPAR`;
-                    colorear.className = "negro";
-                }
-            }else{
-                numRandom2 == 0;
-                document.getElementById("categorizado").innerHTML=`El numero es ${numRandom2} VERDE`;
-                colorear.className = "verde";
-            };
+// Transfiere los numero elegidos al arraySeleccionado.
+let numSelecionado = "";
 
-            // coloca el resultado en pantalla
-            document.getElementById("resultado").innerHTML=numRandom2;
+// Llama a la mesa.
+const mesa = document.getElementById('mesa')
 
-            // guarda el resultado en el localStorage, usa nResultados para no sobreescribir
-            localStorage.setItem(nResultados, JSON.stringify(numRandom2));
-        }      
-        
-        
-        // para seleccionar en el tablero
-        gallery.addEventListener('click', (e) => {
-            console.log(e.target.textContent);
-            e.target.classList.add("selec");
-            numSelecionado = parseInt(e.target.textContent);
-            arraySelecionado.push(numSelecionado);
-        })
-        
+// Selección en la mesa.
+mesa.addEventListener('click', (e) => {
+    console.log(e.target.textContent);
+    e.target.classList.add("selec");
+    numSelecionado = parseInt(e.target.textContent);
+    arraySelecionado.push(numSelecionado);
+})
 
-        let pesos = 100;
-        let nuevoSaldo = pesos;
-        let saldo = document.getElementById("saldo");
-        let resultado_apuesta = document.getElementById("resultado_apuesta");
-        let ganador = 36;
-
-        function validar_jugada(){
-            let consumoFichas = nuevoSaldo - arraySelecionado.length;
-            if (nuevoSaldo <= 0){
-                alert("No tienes saldo para jugar")
-            }else{
-                if ((arraySelecionado.includes(numRandom2) === true)){
-                    saldo.innerHTML = (consumoFichas + ganador);
-                    resultado_apuesta.innerHTML = "Eres un ganador";
-                }else{
-                    saldo.innerHTML = consumoFichas;
-                    resultado_apuesta.innerHTML = "Seguí participando"
-                }
-            }
-        }
-
-// CONTADOR
+// Contador de tiempo en el juego con mensajes.
 let tiempo = document.getElementById("tiempo_restante");
 let mensaje = document.getElementById("mensaje_tiempo");
 let contenedor_resultado = document.getElementById("contenedor_resultado")
-
 let cont = 30;
-
 setInterval(() =>{
     tiempo.innerHTML = cont--;
-
     if(cont == 29){
         mensaje.innerHTML = "Hacé tus apuestas";
         mensaje.classList.add("hace_apuestas")
@@ -96,18 +35,18 @@ setInterval(() =>{
         mensaje.innerHTML = "¡No va más!";
         mensaje.classList.add("no_va_mas");
     }else if (cont == 0){
-        validar_jugada();
-        clearInterval();
+        categorizar();
         tiempo_pagos();
-        galery();
+        validar_jugada();
     }
 },1000)
 
-
-
+// Tiempo de espera y pagos.
 let cont_pagos = 15;
-
 function tiempo_pagos(){
+    mesa_disabled();
+    clearInterval();
+
     setInterval(() =>{
             tiempo.innerHTML = cont_pagos--;
             if(cont_pagos == 14){
@@ -119,9 +58,57 @@ function tiempo_pagos(){
 },1000)
 }
 
-function galery(){
-    gallery.classList.add("gallery_disable");
+// Control de saldos.
+let pesos = 100;
+let nuevoSaldo = pesos;
+let saldo = document.getElementById("saldo");
+let resultado_apuesta = document.getElementById("resultado_apuesta");
+let ganador = 36;
+let numRandom2 = (Math.round(Math.random()*(36-0)+0));
+
+function validar_jugada(){
+    let consumoFichas = nuevoSaldo - arraySelecionado.length;
+    saldo.innerHTML = consumoFichas;
+    if (nuevoSaldo <= 0){
+        alert("No tienes saldo para jugar")
+    }else{
+        if ((arraySelecionado.includes(numRandom2) === true)){
+            saldo.innerHTML = (consumoFichas + ganador);
+            resultado_apuesta.innerHTML = "Eres un ganador";
+        }else{
+            saldo.innerHTML = consumoFichas;
+            resultado_apuesta.innerHTML = "Seguí participando"
+        }
+    }
 }
 
+// Despues del juego desactiva la mesa.
+function mesa_disabled(){
+    mesa.classList.add("gallery_disable");
+}
 
-
+// Muestra el resultado y categoriza.
+function categorizar(){
+    // Llama al numero ganador.
+    numRandom2;        
+    //  Donde se aloja el que sale.
+    const nResultados = [];
+    let colorear = document.getElementById("resultado");
+    if (numRandom2 >= 1 && numRandom2 <= 36){
+        if (numRandom2%2 == 0){
+            document.getElementById("categorizado").innerHTML=`El numero es ${numRandom2} ROJO - PAR`;
+            colorear.className = "rojo";
+        }else{
+            document.getElementById("categorizado").innerHTML=`El numero es ${numRandom2} NEGRO - IMPAR`;
+            colorear.className = "negro";
+        }
+    }else{
+        numRandom2 == 0;
+        document.getElementById("categorizado").innerHTML=`El numero es ${numRandom2} VERDE`;
+        colorear.className = "verde";
+    };
+    // Coloca el resultado en pantalla.
+    document.getElementById("resultado").innerHTML=numRandom2;
+    // Guarda el resultado en el localStorage, usa nResultados para no sobreescribir.
+    localStorage.setItem(nResultados, JSON.stringify(numRandom2));
+}      
